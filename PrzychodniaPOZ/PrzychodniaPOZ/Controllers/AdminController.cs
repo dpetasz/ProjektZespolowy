da91186b625f3be8e202fed0295baa77f8363c9b
Darek Petasz <dpetasz@gmail.com>
2017-12-10 05:10:25 +0100

    Dodałem widok i controler do listy specjalizacji i wyświetla dostępne wizyty po specjalizacji

--- PrzychodniaPOZ/PrzychodniaPOZ/Controllers/AdminController.cs
+++ PrzychodniaPOZ/PrzychodniaPOZ/Controllers/AdminController.cs
@@ -10,6 +10,8 @@ namespace PrzychodniaPOZ.Controllers
 {
     public class AdminController : Controller
     {
+        private PrzychodniaContext db = new PrzychodniaContext();
+
         public ActionResult Index()
         {
             return View();
@@ -41,13 +43,62 @@ namespace PrzychodniaPOZ.Controllers
         }
 
 
-
+        
 
         public ActionResult Wyloguj()
         {
             int user = (int)Session["PracownikID"];
             Session.Abandon();
             return RedirectToAction("Logowanie", "Admin");
+        }
+
+        
+
+
+        
+        public ActionResult SpecjalizacjaMenu()
+        {
+            var specjalizacja = db.Specjalizacja.ToList();
+
+            return View(specjalizacja);
+        }
+
+        
+
+        // GET: WizytaLekarz/DodajWizytaLekarz
+        [HttpGet]
+        public ActionResult DodajWizytaLekarz(string id)
+        {
+            int idSpec = Convert.ToInt32(id);
+            var wizyty =  (from wl in db.WizytaLekarz
+                                where wl.SpecjalizacjaId == idSpec
+                                select wl).ToList();
+
+           
+            
+            /*ViewBag.LekarzId = new SelectList(db.Lekarz, "LekarzId", "Nazwisko");
+            ViewBag.SpecjalizacjaId = new SelectList(db.Specjalizacja, "SpecjalizacjaId", "Nazwa");*/
+            return View(wizyty);
+        }
+
+        // POST: WizytaLekarz/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DodajWizytaLekarz([Bind(Include = "WizytaLekarzId,LekarzId,SpecjalizacjaId,PacjenId,DataWizyty,GodzinaWizyty,Status")] WizytaLekarz wizytaLekarz)
+        {
+            /*if (ModelState.IsValid)
+            {
+                wizytaLekarz.Status = false;
+                db.WizytaLekarz.Add(wizytaLekarz);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LekarzId = new SelectList(db.Lekarz, "LekarzId", "Nazwisko", wizytaLekarz.LekarzId);
+            ViewBag.SpecjalizacjaId = new SelectList(db.Specjalizacja, "SpecjalizacjaId", "Nazwa", wizytaLekarz.SpecjalizacjaId);*/
+            return View(wizytaLekarz);
         }
     }
 }
